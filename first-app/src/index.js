bb2456ad708c0775cd60ddc6def569350b91d1f2
Chris <ctre85@icloud.com>
2023-06-18 00:31:02 +0100

    JSX passing object to submit button

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-    return 'Hi there'
-}
-
-//function-based component
 const App = () => {
-    const buttonText = "Click me!"
+    const buttonText = {text: "Submit"}
     return(
         <div>
             <label for="name" className= 'label'> Enter e-mail</label>
             <input id='name' type='text' />
             <button style={{backgroundColor: 'red', color: 'white'}}>
-                { getButtonText() }
+                { buttonText.text }
             </button>
         </div>
     )
