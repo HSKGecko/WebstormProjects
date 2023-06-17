bccd70f01dc2da3efd78d75bd844c7f548caa086
HSKGecko <support@hskgecko.com>
2023-06-18 00:21:04 +0100

    JSX passing a function

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Hi there'
+}
+
 //function-based component
 const App = () => {
     const buttonText = "Click me!"
     return(
         <div>
-            <label for='name' className= 'label'> Enter e-mail</label>
+            <label for="name" className= 'label'> Enter e-mail</label>
             <input id='name' type='text' />
             <button style={{backgroundColor: 'red', color: 'white'}}>
-                {buttonText}
+                { getButtonText() }
             </button>
         </div>
     )
