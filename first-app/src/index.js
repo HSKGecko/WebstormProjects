ec143f19dbee25f3e951c716102ef95ad6c5f101
Chris <ctre85@icloud.com>
2023-06-18 00:35:40 +0100

    Including style as an object variable

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const buttonText = {text: "Submit"}
+    const style = {backgroundColor: 'red', color: 'white'};
     return(
         <div>
             <label for="name" className= 'label'> Enter e-mail</label>
             <input id='name' type='text' />
-            <button style={{backgroundColor: 'red', color: 'white'}}>
+            <button style = {style}>
                 { buttonText.text }
             </button>
         </div>
     )
 }
-
+//We can't show a javascript object as an attribute on an element as above.
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
