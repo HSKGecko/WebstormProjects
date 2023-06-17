9a22ebaeaf80da7fabbb206a800c369c35f8eff6
Chris <ctre85@icloud.com>
2023-06-18 00:18:36 +0100

    JSX variables
    Inserting an object as text for the submit button

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 //function-based component
 const App = () => {
+    const buttonText = "Click me!"
     return(
         <div>
             <label for='name' className= 'label'> Enter e-mail</label>
             <input id='name' type='text' />
-            <button style={{backgroundColor: 'red', color: 'white'}}>Submit</button>
+            <button style={{backgroundColor: 'red', color: 'white'}}>
+                {buttonText}
+            </button>
         </div>
     )
 }
