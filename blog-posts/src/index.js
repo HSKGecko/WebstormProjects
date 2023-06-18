5826e90f57c790a3be2d1fd40fa39253fd87e575
Chris <ctre85@icloud.com>
2023-06-18 02:12:50 +0100

    Updating index.js with jsx

--- blog-posts/src/index.js
+++ blog-posts/src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     return(
-        <div>
-            Hello World
+        <div className='ui comments'>
+            <div className="comment">
+                <a href="/" className='avatar'>
+                    <img alt="profile picture"/>
+                </a>
+                <div className="content">
+                    <a href="/" className='author'>
+                        Sarah
+                    </a>
+                    <div className="metadata">
+                        <span className='date'>
+                            Today at 5:00PM
+                        </span>
+                        <div className="text">
+                            It's amazing
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
