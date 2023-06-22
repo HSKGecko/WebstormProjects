2d2acbe1271b10c8bbd54d4271d415b0c9616c3c
Chris <ctre85@icloud.com>
2023-06-23 00:50:27 +0100

    Passing components into other child components

--- blog-posts/src/index.js
+++ blog-posts/src/index.js
@@ -4,28 +4,36 @@ import Profile1 from './images/pic1.jpg';
 import Profile2 from './images/pic2.jpg';
 import Profile3 from './images/pic3.jpg';
 import SingleComment from "./SingleComment";
+import UserCard from "./UserCard";
 
 const App = () => {
     return(
         <div className='ui comments'>
-            <SingleComment
-                name='Ethan baby'
-                date='Today at 5pm'
-                text='It is amazing'
-                picture={Profile1}
-            />
-            <SingleComment
-                name='Ethan toddler'
-                date='Today at 6pm'
-                text='Great Job'
-                picture={Profile2}
-            />
-            <SingleComment
-                name='Ethan getting bigger'
-                date='Today at 7pm'
-                text='Thanks'
-                picture={Profile3}
-            />
+            <UserCard>
+                <SingleComment
+                    name='Ethan baby'
+                    date='Today at 5pm'
+                    text='It is amazing'
+                    picture={Profile1}
+                />
+            </UserCard>
+            <UserCard>
+                <SingleComment
+                    name='Ethan toddler'
+                    date='Today at 6pm'
+                    text='Great Job'
+                    picture={Profile2}
+                />
+            </UserCard>
+            <UserCard>
+                <SingleComment
+                    name='Ethan getting bigger'
+                    date='Today at 7pm'
+                    text='Thanks'
+                    picture={Profile3}
+                />
+            </UserCard>
+
         </div>
     )
 }
